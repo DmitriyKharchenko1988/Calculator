27cf0ea93a5f784881f90915b5f460131197aa21
Dmitriy Kharchenko <dm.kharchenko1988@gmail.com>
2023-09-14 21:36:02 +0300

    created calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
+  toShow = '0'
+  currValue = ''
+
+  writeToInput(value:string){
+    this.currValue = this.currValue +  value
+    this.toShow = this.currValue
+  }
+
+  equals() {
+    this.toShow = eval(this.currValue)
+    this.currValue = this.toShow
+  }
+
+  clear() {
+    this.currValue = '';
+    this.toShow = '0'
+  }
+
+  back() {
+    this.currValue = this.currValue.slice(0, -1)
+    this.toShow = this.currValue
+    if (this.toShow == '') {this.toShow = '0'}
+  }
+
 }
